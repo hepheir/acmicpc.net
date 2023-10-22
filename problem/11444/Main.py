80da9db87f5931973098b73827e3ac4499dda7da
Hepheir <hepheir@gmail.com>
2023-10-22 14:06:32 +0900

    AC: O(log N), (N<1,000,000,000,000,000,000), 행렬의 제곱을 이용한 피보나치 계산 (Call by ref로 MLE 우회, N=1에서 예외처리)
    O(log N) 알고리즘 참고: https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
    
    Modulo의 분배, 결합법칙 참고: https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-multiplication

--- problem/11444/Main.py
+++ problem/11444/Main.py
@@ -1,7 +1,5 @@
-import sys
-
-
 MOD = 1000000007
+FIBO_MAT = [[1,1],[1,0]]
 
 
 def mod_mult(a: int, b: int, m: int = MOD) -> int:
@@ -12,37 +10,40 @@ def mod_add(a: int, b: int, m: int = MOD) -> int:
     return ((a % m) + (b % m)) % m
 
 
-def m_pow(exp: int) -> list[list[int]]:
+def m_copy(mat: list[list[int]]) -> list[list[int]]:
+    return [[mat[0][0], mat[0][1]], [mat[1][0], mat[1][1]]]
+
+
+def m_pow(mat: list[list[int]], exp: int):
     if exp == 1:
-        return [[1,1],[1,0]]
-    mat = m_pow(exp//2)
-    mat = m_mult(mat, mat)
+        return
+    m_pow(mat, exp//2)
+    m_mult(mat, mat)
     if exp % 2 == 1:
-        mat = m_mult(mat, m_pow(1))
-    return mat
+        m_mult(mat, FIBO_MAT)
 
 
-def m_mult(A: list[list[int]], B: list[list[int]]) -> list[list[int]]:
-    return [
-        [
-            mod_add(mod_mult(A[0][0], B[0][0]), mod_mult(A[0][1], B[1][0])),
-            mod_add(mod_mult(A[0][0], B[0][1]), mod_mult(A[0][1], B[1][1])),
-        ],
-        [
-            mod_add(mod_mult(A[1][0], B[0][0]), mod_mult(A[1][1], B[1][0])),
-            mod_add(mod_mult(A[1][0], B[0][1]), mod_mult(A[1][1], B[1][1])),
-        ],
-    ]
+def m_mult(mat: list[list[int]], other: list[list[int]]):
+    a = mod_add(mod_mult(mat[0][0], other[0][0]), mod_mult(mat[0][1], other[1][0]))
+    b = mod_add(mod_mult(mat[0][0], other[0][1]), mod_mult(mat[0][1], other[1][1]))
+    c = mod_add(mod_mult(mat[1][0], other[0][0]), mod_mult(mat[1][1], other[1][0]))
+    d = mod_add(mod_mult(mat[1][0], other[0][1]), mod_mult(mat[1][1], other[1][1]))
+    mat[0][0] = a
+    mat[0][1] = b
+    mat[1][0] = c
+    mat[1][1] = d
 
 
 def fib(n: int) -> int:
     if n == 0:
         return 0
-    return m_pow(n-1)[0][0] % MOD
+    if n == 1:
+        return 1
+    fibo_mat = m_copy(FIBO_MAT)
+    m_pow(fibo_mat, n-1)
+    return fibo_mat[0][0]
 
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(int(1e7))
-
-    n = int(sys.stdin.readline())
+    n = int(input())
     print(fib(n))
