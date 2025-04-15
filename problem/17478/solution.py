a51a1d49efccc5b76e733c5e611587cd819386d3
Hepheir <hepheir@gmail.com>
2025-04-15 22:53:42 +0900

    AC(32ms): O(N) #recursive #string

---
+++ problem/17478/solution.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def print_recursive(depth: int, prefix: str = ''):
+    if not prefix:
+        sys.stdout.write('''어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n''')
+
+    sys.stdout.write(prefix+'''"재귀함수가 뭔가요?"\n''')
+    if depth == 0:
+        sys.stdout.write(prefix+'''"재귀함수는 자기 자신을 호출하는 함수라네"\n''')
+    else:
+        sys.stdout.write(prefix+'''"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n''')
+        sys.stdout.write(prefix+'''마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n''')
+        sys.stdout.write(prefix+'''그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."\n''')
+        print_recursive(depth-1, prefix+'____')
+    sys.stdout.write(prefix+'''라고 답변하였지.\n''')
+
+
+N = int(input())
+print_recursive(N)
