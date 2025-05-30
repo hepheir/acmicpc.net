53e2ee071a6bc4a20efbbd52e35edbbf87e597b5
Hepheir <hepheir@gmail.com>
2025-05-31 02:37:57 +0900

    AC(428ms): O(N log N) #sorting #greedy
    sorting 시간 복잡도를 제외하면 O(N)

---
+++ problem/21599/Main.py
@@ -0,0 +1,17 @@
+N = int(input())
+A = sorted(map(int, input().split()), reverse=True)
+
+did_upgrade = [0] * N
+
+to_upgrade = 0
+for i in range(N):
+    # TODO: 자기 자신도 강화대상에 포함되는지 확인하기
+    if to_upgrade:
+        did_upgrade[i] = 1
+        to_upgrade -= 1
+    to_upgrade = max(to_upgrade, A[i])
+
+for i in range(to_upgrade):
+    did_upgrade[i] = 1
+
+print(sum(did_upgrade))
