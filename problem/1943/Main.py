ac730f3c25c0f3716221c1d1f53cc170ba29079b
Hepheir <hepheir@gmail.com>
2025-06-17 00:01:24 +0900

    TLE(44%): #bfs #bruteforce

--- problem/1943/Main.py
+++ problem/1943/Main.py
@@ -1,35 +1,36 @@
 from typing import *
-from functools import cache
+from collections import deque
 import sys
 
 
-COIN: List[Tuple[int, int]] = [] # [(unit, count), ...]
+MAX_CAPACITY = 100000
 
-
-@cache
-def knapsack(i: int, capacity: int) -> bool:
-    if capacity < 0:
-        return False
-    if i == len(COIN):
-        return capacity == 0
-    unit, max_count = COIN[i]
-    for count in reversed(range(min(max_count, capacity//unit)+1)):
-        if knapsack(i+1, capacity-unit*count):
-            return True
-    return False
-
+queue = deque()
+visited = [False] * (MAX_CAPACITY+1)
 
 for _ in range(3):
-    knapsack.cache_clear()
-    COIN.clear()
     N = int(sys.stdin.readline())
-    total_balance = 0
+
+    for n in range(N//2+1):
+        visited[n] = False
+    queue.clear()
+
+
+    capacity = 0
+    queue.append(0)
+
     for i in range(N):
         unit, count = map(int, sys.stdin.readline().split())
-        total_balance += unit * count
-        COIN.append((unit, count))
-    COIN.sort(reverse=True)
-    if (total_balance % 2 == 0) and knapsack(0, total_balance//2):
+        capacity += unit * count
+        for _ in range(count):
+            for _ in range(len(queue)):
+                x = queue.popleft()
+                y = x+unit
+                if not visited[y]:
+                    visited[y] = True
+                    queue.append(y)
+                queue.append(x)
+    if capacity % 2 == 0 and visited[capacity//2]:
         sys.stdout.write("1\n")
     else:
         sys.stdout.write("0\n")
