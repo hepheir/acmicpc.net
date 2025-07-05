c63c8dc2493cffbc6cb2859fe7cf2abcd2482a51
Hepheir <hepheir@gmail.com>
2025-07-06 02:35:22 +0900

    WA(9%): #caseworks #greedy

---
+++ problem/20292/Main.py
@@ -0,0 +1,62 @@
+from collections import defaultdict
+import sys
+
+
+READ = defaultdict(int)
+WRITE = defaultdict(int)
+
+time = 0
+last_wait = 0
+
+
+def exec_READ(A: int):
+    global last_wait, time
+    # check READ with WRITE
+    if WRITE[A] > last_wait:
+        exec_WAIT()
+    READ[A] = time
+    sys.stdout.write(f'READ {A}\n')
+    time += 1
+
+
+def exec_WRITE(A: int, B: int):
+    global last_wait, time
+    # check READ with WRITE
+    if READ[B] > last_wait:
+        exec_WAIT()
+    if WRITE[A] > last_wait:
+        exec_WAIT()
+    if WRITE[B] > last_wait:
+        exec_WAIT()
+    READ[A] = time
+    WRITE[B] = time
+    sys.stdout.write(f'WRITE {A} TO {B}\n')
+    time += 1
+
+
+def exec_WAIT():
+    global last_wait, time
+    last_wait = time
+    sys.stdout.write('WAIT\n')
+    time += 1
+
+
+def exec_EXIT():
+    sys.stdout.write('EXIT\n')
+
+
+while True:
+    stmt = sys.stdin.readline().strip()
+    tokens = stmt.split()
+    if tokens[0] == 'READ':
+        A = tokens[1]
+        exec_READ(A)
+        continue
+    if tokens[0] == 'WRITE':
+        A = tokens[1]
+        B = tokens[3]
+        exec_WRITE(A, B)
+        continue
+    if tokens[0] == 'EXIT':
+        exec_EXIT()
+        break
