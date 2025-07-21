fb4960b2af51cd468ff0bbd7cf793e8542dc0b1e
Hepheir <hepheir@gmail.com>
2025-07-21 23:41:55 +0900

    AC(36ms): O(N) #math #arithmetics

--- problem/15216/Main.py
+++ problem/15216/Main.py
@@ -6,22 +6,18 @@ import sys
 H, W, N = map(int, sys.stdin.readline().split())
 X = list(map(int, sys.stdin.readline().split()))
 
-def solve() -> bool:
-    w = W
-    h = H
-    for i in range(N):
-        w -= X[i]
-        if w == 0:
-            w = W
-            h -= 1
-            if h == 0:
-                return True
-        if w < 0:
-            return False
-    return False
+answer = 'NO'
+w = W
+h = H
+for i in range(N):
+    w -= X[i]
+    if w == 0:
+        w = W
+        h -= 1
+        if h == 0:
+            answer = 'YES'
+            break
+    if w < 0:
+        break
 
-
-if solve():
-    print('YES')
-else:
-    print('NO')
+print(answer)
