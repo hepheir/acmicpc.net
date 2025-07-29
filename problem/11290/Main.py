6cafb579cec682695e4dc1eee6e49274b68a380e
Hepheir <hepheir@gmail.com>
2025-07-30 02:54:45 +0900

    AC(872ms): [S2] O(n π(n)) #sieve_of_eratosthenes #bruteforcing #number_theory
    wonowon 숫자의 길이는 O(N)으로, 최대 10,000 자리 숫자까지 가면 너무 커지니, 두 자리 늘릴 때(x100+1) 마다 바로 바로 mod p 해주지 않고 Big Int로 처리하려고 해도 시간초과를 피할 수 없습니다.
    즉, 정리하면 modulo가 적용된 사칙연산에 대하여 n 이하의 소수에 대해 bruteforcing 해주어야 하는 문제.
    이 때의 시간 복잡도는 O(n * π(n)) 로 보이며, 실버 하위권에 있을 난이도는 아니라고 생각합니다.
    
    *π(n): n 이하인 소수의 개수.

--- problem/11290/Main.py
+++ problem/11290/Main.py
@@ -1,35 +1,40 @@
 # 11290번: Wonowon
 
-
-def get_primes(hi: int):
-    sieve = [True] * (n + 1)
-    sieve[0] = False
-    sieve[1] = False
-    for i in range(2, int(hi**0.5)+1):
-        if sieve[i]:
-            for j in range(i*i, hi+1, i):
-                sieve[j] = False
-    for i in range(2, hi+1):
-        if sieve[i]:
-            yield i
+import sys
+import collections
 
 
-def W(p: int, hi: int) -> int:
-    number = 1
-    length = 1
-    while length <= hi:
-        if number % p == 0:
-            return length
-        number = number * 100 + 1
-        length += 2
-    return -1
+N = int(sys.stdin.readline())
 
+primes = []
+sieve = [True] * (N+1)
+sieve[0] = False
+sieve[1] = False
+for i in range(2, int(N**0.5)+1):
+    if sieve[i]:
+        for j in range(i*i, N+1, i):
+            sieve[j] = False
+for i in range(2, N+1):
+    if sieve[i]:
+        primes.append(i)
 
-if __name__ == '__main__':
-    n = int(input())
-    answer = 0
-    for p in get_primes(n):
-        if W(p, p-2) == p-2:
-            answer += 1
+queue = collections.deque()
 
-    print(answer)
+for p in primes:
+    queue.append((1, p))
+
+answer = 0
+w_p = 1
+while queue:
+    for _ in range(len(queue)):
+        w, p = queue.popleft()
+        if w % p == 0:
+            if w_p == p - 2:
+                answer += 1
+            continue
+        if w_p > p - 2:
+            continue
+        queue.append(((w*100+1)%p, p))
+    w_p += 2
+
+print(answer)
