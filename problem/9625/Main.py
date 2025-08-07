53853ae92db0c6b1e6f6713472e785a79d060ac8
Hepheir <hepheir@gmail.com>
2025-08-08 06:00:43 +0900

    AC(36ms): O(K) #mathematics #simulation
    [S5]
    naive하게 문자열을 직접 바꿔가면서 implementation+simulation 하면 MLE를 받아버리네요. 머릿속으로 몇 번의 시뮬레이션을 해본 후, A, B 개수만 세어도 무방하다는 점을 깨닫고 점화식을 세웠는데, 피보나치여서 좋은 의미로 어이 없었던 문제입니다.
    
    // 결국 for 문을 이용하여 a, b = b, (a+b) 를 K번 반복하여 풀이하게 되었는데, 이는 동적 프로그래밍이라고 보긴 어렵고 시뮬레이션으로 보는게 좀 더 맞을 것 같습니다. dp에 비해 훨씬 구현이 쉽기도 하구요.
    
    → 가장 쉬운 풀이를 기준으로 태깅+난이도 투표를 한다는 원칙을 기준으로 math, simulation 태그를 붙입니다.

--- problem/9625/Main.py
+++ problem/9625/Main.py
@@ -2,25 +2,11 @@
 
 K = int(input())
 
-curr = ['A']
-prev = []
-for _ in range(K):
-    curr, prev = prev, curr
-    curr.clear()
-    for c in prev:
-        if c == 'A':
-            curr.append('B')
-        else:
-            curr.append('B')
-            curr.append('A')
-
-A_count = 0
+A_count = 1
 B_count = 0
 
-for c in curr:
-    if c == 'A':
-        A_count += 1
-    else:
-        B_count += 1
+for _ in range(K):
+    # 뭔가 피보나치 같네
+    A_count, B_count = B_count, B_count+A_count
 
 print(A_count, B_count)
