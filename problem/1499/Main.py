50608cffb2bdfde33f64013aba75f27c848b513a
Hepheir <hepheir@gmail.com>
2025-08-15 08:58:19 +0900

    WA: O(N^2) #two_pointer #greedy
    문제의 조건에 따라 r(i1,j1), r(i2,j2), ... r(im,jm) 이라면, i1 ≤ i2 ≤ ... ≤ im이고, j1 ≥ j2 ≥ ... ≥ jm이도록 할 수 있도록 i, j의 범위를 좁혀오며 그리디하게 뒤집기를 수행했지만 이 풀이는 틀렸습니다.

--- problem/1499/Main.py
+++ problem/1499/Main.py
@@ -1,24 +1,28 @@
 # 1499번: 뒤집기 수열
 
-A = list(input().strip())
-B = list(input().strip())
-N = len(A)
+MAX_N = 50
+INF = MAX_N+1
 
-swap_count = 0
-i = 0
-j = N-1
-while i <= j:
-    while i < j and A[j] == B[j]:
-        j -= 1
-    if A[i] != B[i]:
-        for k in reversed(range(i+1, j+1)):
-            if A[k] == B[i]:
-                A[i:k+1] = A[i:k+1][::-1]
-                swap_count += 1
-                break
-        else:
-            print(-1)
-            break
-    i += 1
-else:
-    print(swap_count)
+
+def r(A: list, B: list) -> int:
+    i = 0
+    j = len(A)
+    swap_count = 0
+    while i < j-1:
+        while i < j and A[i] == B[i]:
+            i += 1
+        while i < j and A[j-1] == B[j-1]:
+            j -= 1
+        if i < j-1:
+            if A[i] == A[j-1]:
+                return -1
+            A[i:j] = A[i:j][::-1]
+            swap_count += 1
+    return swap_count if A[i:j] == B[i:j] else -1
+
+
+if __name__ == '__main__':
+    A = list(input().strip())
+    B = list(input().strip())
+    answer = r(A, B)
+    print(answer)
