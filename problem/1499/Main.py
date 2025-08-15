d679c5560effa419829d12f092333f63b920616b
Hepheir <hepheir@gmail.com>
2025-08-15 11:36:01 +0900

    AC(116ms): O(N^3) #bruteforcing #dp
    [G1]
    그래프 탐색을 하던, 아무튼 브루트포스를 하면 되는 문제. 하지만 naive bruteforce 는 TLE를 받기 때문에, 적절히 메모이즈 하여 시간복잡도를 O(|A|^3)로 단축시켜야 합니다.
    
    BF+DP 풀이가 옮음을 증명하기가 조금 어려웠던 것 같아요. 체감 난이도는 G2~G1

--- problem/1499/Main.py
+++ problem/1499/Main.py
@@ -1,28 +1,46 @@
 # 1499번: 뒤집기 수열
 
+import functools
+
 MAX_N = 50
 INF = MAX_N+1
 
-
-def r(A: list, B: list) -> int:
-    i = 0
-    j = len(A)
-    swap_count = 0
-    while i < j-1:
-        while i < j and A[i] == B[i]:
-            i += 1
-        while i < j and A[j-1] == B[j-1]:
-            j -= 1
-        if i < j-1:
-            if A[i] == A[j-1]:
-                return -1
-            A[i:j] = A[i:j][::-1]
-            swap_count += 1
-    return swap_count if A[i:j] == B[i:j] else -1
+A = input().strip()
+B = input().strip()
+N = len(A)
 
 
-if __name__ == '__main__':
-    A = list(input().strip())
-    B = list(input().strip())
-    answer = r(A, B)
-    print(answer)
+def r(A: str, i: int, j: int) -> str:
+    # O(N)
+    A = list(A)
+    A[i:j] = reversed(A[i:j])
+    return ''.join(A)
+
+
+def solve() -> int:
+    if (ans := min(solve_util(A, lo=0, hi=N), default=INF)) < INF:
+        return ans
+    else:
+        return -1
+
+
+@functools.cache
+def solve_util(A: str, lo: int, hi: int) -> list:
+    # O(N^2 x N) = O(N^3)
+    answer = []
+    if lo == hi:
+        answer.append(0)
+    else:
+        if A[lo] == B[lo]:
+            answer.append(min(solve_util(A, lo+1, hi), default=INF))
+        if A[hi-1] == B[hi-1]:
+            answer.append(min(solve_util(A, lo, hi-1), default=INF))
+        A = r(A, lo, hi)
+        if A[lo] == B[lo]:
+            answer.append(min(solve_util(A, lo+1, hi), default=INF)+1)
+        if A[hi-1] == B[hi-1]:
+            answer.append(min(solve_util(A, lo, hi-1), default=INF)+1)
+    return answer
+
+
+print(solve())
