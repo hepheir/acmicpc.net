4e8fae8a2b344f2fb93097e71d288e15d37ed084
Hepheir <hepheir@gmail.com>
2025-08-31 02:40:16 +0900

    WA(3%): O(n log n) #priority_queue
    [G1]
    최소힙, 최대힙을 하나 씩 관리하되, 수명이 지난 값들을 제거해주는 방식으로 구현. (했으나 틀림.. 왜?)

---
+++ problem/2433/Main.py
@@ -0,0 +1,34 @@
+# 2433번: The Sound of Silence
+
+import sys
+import heapq
+
+
+n, m, c = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+
+max_heap = []
+min_heap = []
+
+silence = []
+
+i = 0
+while i < n:
+    heapq.heappush(min_heap, (a[i], i))
+    heapq.heappush(max_heap, (-a[i], i))
+
+    while max_heap[0][1] <= i-m:
+        heapq.heappop(max_heap)
+    while min_heap[0][1] <= i-m:
+        heapq.heappop(min_heap)
+
+    if i >= m:
+        if abs(min_heap[0][0]+max_heap[0][0]) <= c:
+            silence.append(i)
+    i += 1
+
+
+if not silence:
+    print('NONE')
+else:
+    print('\n'.join(map(str, silence)))
