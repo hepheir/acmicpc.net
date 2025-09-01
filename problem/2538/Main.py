79bb0132da04e27fae6dcc9a87bd2217425fd5b5
Hepheir <hepheir@gmail.com>
2025-09-02 05:01:30 +0900

    RE(3%, ValueError): O(|V|) #geometry #case_works
    [P5]
    대체 무슨 예외가 있는거지...

--- problem/2538/Main.py
+++ problem/2538/Main.py
@@ -6,43 +6,69 @@ import sys
 
 def solve(W: int, H: int, vertex: List[Tuple[int, int]]) -> int:
     for x, y in vertex:
-        # 다각형의 변이 테두리에 닿는지 확인하기.
         if x == 0 or x == W or y == 0 or y == H:
             break
     else:
-        perimeter = 2*(W+H) + calc_perimeter(vertex)
+        # 색종이가 나뉘지 않는 경우.
+        perimeter = 2*(W+H) + calc_polygon_perimeter(vertex)
         return 1, perimeter
 
-    perimeters = []
-    vertex_segment = get_vertex_segment(W, H, vertex)
-    for i in range(len(vertex_segment)):
-        if len(vertex_segment[i]) < 2:
-            continue
-        polygon = close_polygon(W, H, vertex_segment[i])
-        perimeter = calc_perimeter(polygon)
-        if perimeter > 0:
-            perimeters.append(perimeter)
-
+    perimeters = [*map(calc_polygon_perimeter, get_polygons(W, H, vertex))]
+    if not perimeters:
+        return 0, 0
     return len(perimeters), max(perimeters)
 
 
-def get_vertex_segment(W: int, H: int, vertex: List[Tuple[int, int]]) -> List[List[Tuple[int, int]]]:
-    vertex_segment: List[List[Tuple[int, int]]] = [[]]
-    for s in range(len(vertex)):
-        x, y = vertex[s]
-        if x == 0 or x == W or y == 0 or y == H:
-            break
-    for i in range(s, s+len(vertex)+1):
-        i %= len(vertex)
-        x, y = vertex[i]
-        if x == 0 or x == W or y == 0 or y == H:
-            vertex_segment[-1].append(vertex[i])
-            vertex_segment.append([])
-        vertex_segment[-1].append(vertex[i])
-    return vertex_segment
+def get_polygons(W: int, H: int, vertex: List[Tuple[int, int]]) -> List[List[Tuple[int, int]]]:
+    corner = [(0, 0), (W, 0), (W, H), (0, H)]
+
+    def is_end_of_edge(i: int) -> bool:
+        cx, cy = vertex[i]
+        px, py = vertex[i-1]
+        dx, dy = cx-px, cy-py
+        return (dx == 0 and cx*(cx-W) == 0) or (dy == 0 and cy*(cy-H) == 0)
+
+    def partition_vertices(start: int) -> List[List[Tuple[int, int]]]:
+        partition: List[List[Tuple[int, int]]] = [[]]
+        for i in range(start, start+len(vertex)):
+            i %= len(vertex)
+            if not is_end_of_edge(i):
+                partition[-1].append(vertex[i])
+            elif len(partition[-1]) <= 1:
+                partition[-1].clear()
+                partition[-1].append(vertex[i])
+            else:
+                partition.append([vertex[i]])
+        return partition
+
+    def close_polygon(vertex: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+        "색종이의 꼭짓점을 포함하여 다각형의 모든 정점을 표시한다."
+        vertex = vertex.copy()
+        # 첫 번째 코너 찾기
+        dx, dy = (vertex[-1][0]-vertex[-2][0]), (vertex[-1][1]-vertex[-2][1])
+        if dx > 0: i = 1
+        if dx < 0: i = 3
+        if dy > 0: i = 2
+        if dy < 0: i = 0
+        # 3개 이상의 변을 색종이와 공유하는 경우 예외처리
+        if abs(vertex[0][0]-vertex[-1][0]) * abs(vertex[0][1]-vertex[-1][1]) == 0:
+            vertex.append(corner[i])
+            i -= 1
+        while True:
+            vertex.append(corner[i])
+            if (corner[i][0] == vertex[0][0]) or (corner[i][1] == vertex[0][1]):
+                break
+            i -= 1
+        return vertex
+
+    for i in range(len(vertex)):
+        if is_end_of_edge(i):
+            return [*map(close_polygon, partition_vertices(i))]
+    else:
+        return [vertex]
 
 
-def calc_perimeter(polygon: List[Tuple[int, int]]) -> int:
+def calc_polygon_perimeter(polygon: List[Tuple[int, int]]) -> int:
     perimeter = 0
     for i in range(len(polygon)):
         cx, cy = polygon[i]
@@ -50,36 +76,6 @@ def calc_perimeter(polygon: List[Tuple[int, int]]) -> int:
         dx, dy = cx-px, cy-py
         perimeter += abs(dx)+abs(dy)
     return perimeter
-
-
-def close_polygon(W: int, H: int, vertex: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    # 다각형이 아니면 빈 정점 리스트를 반환.
-    if len(vertex) <= 1:
-        return []
-    if len(vertex) == 2:
-        cx, cy = vertex[0]
-        nx, ny = vertex[1]
-        dx, dy = nx-cx, ny-cy
-        if dx == 0 and (cx == 0 or cx == W):
-            return []
-        if dy == 0 and (cy == 0 or cy == H):
-            return []
-    vertex = vertex.copy()
-    px, py = vertex[-2]
-    cx, cy = vertex[-1]
-    dx, dy = cx-px, cy-py
-    if dx:
-        vertex.append((W, 0) if dx > 0 else (0, H))
-    if dy:
-        vertex.append((W, H) if dy > 0 else (0, 0))
-    cx, cy = vertex[0]
-    nx, ny = vertex[1]
-    dx, dy = nx-cx, ny-cy
-    if dx:
-        vertex.append((0, 0) if dx > 0 else (W, H))
-    if dy:
-        vertex.append((W, 0) if dy > 0 else (0, H))
-    return vertex
 
 
 def main():
