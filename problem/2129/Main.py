84fd0b433bb8a87efc58aa2328f26469380600cc
Hepheir <hepheir@gmail.com>
2025-09-15 03:44:13 +0900

    WA(89%): O(NM) #bellman_ford #dag #graph_traversal
    [P5]
    음수사이클 검사도 했고, 방문가능한 경로상에 음수 사이클이 있는지도 확인했다. 왜 틀렸을까...

--- problem/2129/Main.py
+++ problem/2129/Main.py
@@ -1,76 +1,138 @@
 # 2129번: 드라이브 파이널
 
+from typing import *
 import sys
 
 
 INF = sys.maxsize
 
 
-def solve(N: int, M: int, S: int, T: int, G: list) -> str:
-    edges = []
-    for u in range(N):
-        if not G[u]:
-            continue
-        min_c, d, v = min(G[u])
-        for i in range(len(G[u])):
-            c, d, v = G[u][i]
-            if min_c == c:
-                edges.append((u, v, c, d))
+def solve(N: int, M: int, S: int, T: int, E: List[Tuple[int, int, int, int, int]]) -> str:
+    graph, vertices, source, sink = init_directed_graph(N, M, S, T, E)
+    graph = filter_edges(graph, vertices)
+    used_vertices = filter_vertices(graph, vertices, source, sink)
+    distance, fatigue, is_negative_cycle = bellman_ford(graph, vertices)
 
-    # Bellman-ford
-
-    dist = [INF] * N
-    prev = [[] for _ in range(N)]
-    cost = [INF] * N
-
-    dist[S] = 0
-    cost[S] = 0
-
-    for _ in range(N):
-        for u, v, c, d in edges:
-            if cost[v] < cost[u] + c:
-                continue
-            if cost[v] > cost[u] + c:
-                cost[v] = cost[u] + c
-                dist[v] = dist[u] + d
-                prev[v].clear()
-            if dist[v] > dist[u] + d:
-                dist[v] = dist[u] + d
-            prev[v].append(u)
-
-    if dist[T] == INF:
+    if distance[sink] == INF:
         return 'VOID'
 
-    visited = [False] * N
+    for u in vertices:
+        if used_vertices[u] and is_negative_cycle[u]:
+            return 'UNBOUND'
+
+    return f'{fatigue[sink]} {distance[sink]}'
+
+
+def init_directed_graph(N: int, M: int, S: int, T: int, E: List[Tuple[int, int, int, int, int]]) -> Tuple[Dict[int, List[Tuple[int, int, int]]], List[int]]:
+    """인접 리스트 생성"""
+    vertices = list(range(N))
+    source = S
+    sink = T
+    sink_actual = T
+    if source == sink:
+        sink = N
+        vertices.append(sink)
+    graph = {u: [] for u in vertices}
+    for i in range(M):
+        u, v, fatigue_uv, distance, fatigue_vu = E[i]
+        graph[u].append((v, fatigue_uv, distance))
+        graph[v].append((u, fatigue_vu, distance))
+        if u == sink_actual and sink_actual != sink:
+            graph[sink].append((v, fatigue_uv, distance))
+            graph[v].append((sink, fatigue_vu, distance))
+        if v == sink_actual and sink_actual != sink:
+            graph[u].append((sink, fatigue_uv, distance))
+            graph[sink].append((u, fatigue_vu, distance))
+    return graph, vertices, source, sink
+
+
+def filter_edges(graph: Dict[int, List[Tuple[int, int, int]]], vertices: List[int]) -> Dict[int, List[Tuple[int, int, int]]]:
+    """이동 가능한 도로만 남기기"""
+    retval = {u: [] for u in vertices}
+    for u in vertices:
+        if not graph[u]:
+            continue
+        min_fatigue = min(graph[u], key=lambda e: e[1])[1]
+        for v, fatigue, distance in graph[u]:
+            if fatigue == min_fatigue:
+                retval[u].append((v, fatigue, distance))
+    return retval
+
+
+def filter_vertices(graph: Dict[int, List[Tuple[int, int, int]]], vertices: List[int], source: int, sink: int) -> Dict[int, bool]:
     stack = []
-    visited[T] = True
-    stack.append(T)
+    # Graph Traverse
+    visited = {u: False for u in vertices}
+    stack.append(source)
+    visited[source] = True
     while stack:
         u = stack.pop()
-        for v in prev[u]:
+        for v, fatigue, distance in graph[u]:
             if not visited[v]:
                 visited[v] = True
                 stack.append(v)
+    # Reversed Graph Traverse
+    graph_rev = {u: [] for u in vertices}
+    for u in vertices:
+        for v, fatigue, distance in graph[u]:
+            graph_rev[v].append((u, fatigue, distance))
+    visited_rev = {u: False for u in vertices}
+    stack.append(sink)
+    visited_rev[sink] = True
+    while stack:
+        u = stack.pop()
+        for v, fatigue, distance in graph_rev[u]:
+            if not visited_rev[v]:
+                visited_rev[v] = True
+                stack.append(v)
+    return {u: (visited[u] and visited_rev[u]) for u in vertices}
 
-    for _ in range(N):
-        for u, v, c, d in edges:
-            if (cost[v] > cost[u] + c) and visited[v]:
-                cost[v] = cost[u] + c
-                dist[v] = dist[u] + d
-                return 'UNBOUND'
-            if (dist[v] > dist[u] + d) and visited[v]:
-                dist[v] = dist[u] + d
-                return 'UNBOUND'
 
-    return f'{cost[T]} {dist[T]}'
+def bellman_ford(graph: Dict[int, List[Tuple[int, int, int]]], vertices: List[int]):
+    # Bellman-ford, O(VE)
+    edges = []
+    for u in vertices:
+        for v, f, d in graph[u]:
+            edges.append((u, v, f, d))
+
+    distance = {u: INF for u in vertices}
+    fatigue = {u: INF for u in vertices}
+    is_negative_cycle = {u: False for u in vertices}
+
+    distance[S] = 0
+    fatigue[S] = 0
+
+    # Repeat relaxation
+    for _ in range(len(vertices)):
+        for u, v, f, d in edges:
+            if (fatigue[v] > fatigue[u] + f):
+                fatigue[v] = fatigue[u] + f
+                distance[v] = distance[u] + d
+                continue
+            if (fatigue[v] == fatigue[u] + f) and (distance[v] > distance[u] + d):
+                distance[v] = distance[u] + d
+                continue
+
+    # Find negative cycle
+    for _ in range(len(vertices)):
+        for u, v, f, d in edges:
+            if (fatigue[v] > fatigue[u] + f):
+                fatigue[v] = fatigue[u] + f
+                distance[v] = distance[u] + d
+                is_negative_cycle[v] = True
+                continue
+            if (fatigue[v] == fatigue[u] + f) and (distance[v] > distance[u] + d):
+                distance[v] = distance[u] + d
+                continue
+
+    return distance, fatigue, is_negative_cycle
 
 
 if __name__ == '__main__':
     N, M, S, T = map(int, sys.stdin.readline().split())
-    G = [[] for _ in range(N)]
+    E = []
     for _ in range(M):
         u, v, a, c, b = map(int, sys.stdin.readline().split())
-        G[u].append((a, c, v))
-        G[v].append((b, c, u))
-    answer = solve(N, M, S, T, G)
+        E.append((u, v, a, c, b))
+    answer = solve(N, M, S, T, E)
     print(answer)
