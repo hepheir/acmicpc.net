348045ad60eb2c5fcbc2c16351b0b9f42cdfa9d0
Hepheir <hepheir@gmail.com>
2025-09-24 03:44:07 +0900

    WA(1%): #sorting #two_pointer
    선택 정렬로 목표를 미리 만들어두고, 투 포인터로 보고정렬해나가는 방식

--- problem/18192/bogoSort.cpp
+++ problem/18192/bogoSort.cpp
@@ -3,5 +3,24 @@
 
 void sort_array(int N)
 {
-	shuffle_array(0, N - 1);
+	int i, j;
+	std::vector<int> dest, curr;
+	curr = copy_array();
+	for (i = 0; i < N; i++)
+		dest.push_back(curr.at(i));
+	// Selection Sort: O(N^2)
+	for (i = 0; i < N; i++)
+		for (j = i+1; j < N; j++)
+			if (dest.at(i) > dest.at(j))
+				std::swap(dest[i], dest[j]);
+	// Bogo Sort
+	i = 0;
+	j = N-1;
+	while (i < j)
+	{
+		shuffle_array(i, j);
+		curr = copy_array();
+		while (curr.at(i) == dest.at(i) && i+1 < N) i++;
+		while (curr.at(j) == dest.at(j) && 0 <= j-1) j--;
+	}
 }
