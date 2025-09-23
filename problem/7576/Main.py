b6a21b4be3fa034153916d9658864312e4d0d8e9
Hepheir <hepheir@gmail.com>
2025-09-24 05:44:09 +0900

    AC(3644ms): O(MN) #bfs #grid_graph #shortest_path
    [G5]
    BFS를 최단경로 알고리즘으로 사용하고, 익은 토마토들이 출발점이 될 때, 최단 거리들 중 가장 긴 거리가 답이 된다.
    단, 방문하지 못한 토마토가 있을 경우 -1을 출력해주면 되는 문제.
    
    ---
    
    기존에 풀이한 이력이 있으나, 이상원에게 멘토링해주기 위해 다시 풀이하였다.

--- problem/7576/Main.py
+++ problem/7576/Main.py
@@ -1,53 +1,55 @@
+# 7576번: 토마토
+
 import collections
 import sys
 
 
-FRESH = '0'
-RIPEN = '1'
-EMPTY = '-1'
+INF = sys.maxsize
 
-DY = (-1, 0, 0, 1)
-DX = (0, -1, 1, 0)
+TOMATO_RIPEN = '1'
+TOMATO_RAW = '0'
+TOMATO_EMPTY = '-1'
+
+DX = (0, 0, 1, -1)
+DY = (1, -1, 0, 0)
 
 
 M, N = map(int, sys.stdin.readline().split())
-graph = [ sys.stdin.readline().split() for _ in range(N) ]
 
-left = 0 # 앞으로 익어야 할 토마토의 개수
-queue = collections.deque([]) # 갓 익은 토마토의 좌표들
+queue = collections.deque()  # 갓 익었을 때.
+should_be_visited = collections.defaultdict(bool)
+raw_tomato_count = 0
 
-# 갓 익은 토마토들의 좌표를 조사하여 스택에 저장.
-# 아직 익지 않은 토마토들의 개수도 센다.
 for y in range(N):
+    row = sys.stdin.readline().split()
     for x in range(M):
-        if graph[y][x] == FRESH:
-            left += 1
-        elif graph[y][x] == RIPEN:
-            queue.append((y,x))
+        if row[x] == TOMATO_RIPEN:
+            should_be_visited[y, x] = False
+            queue.append((y, x))
+        if row[x] == TOMATO_RAW:
+            should_be_visited[y, x] = True
+            raw_tomato_count += 1
+        if row[x] == TOMATO_EMPTY:
+            should_be_visited[y, x] = False
 
-# BFS에서는 큐의 너비는 어제 갓 익은 토마토의 개수이고
-# 탐색 깊이는 날짜가 얼마나 지났는지를 의미한다.
-width = len(queue)
-depth = -1 # 처음 밭의 상태를 적용했을 때 0일차 종료가 되게 하기 위하여 -1 부터 시작
+
+day = -1
 while queue:
-    y,x = queue.popleft()
-
-    for dy, dx in zip(DY, DX):
-        ny, nx = y+dy, x+dx
-        if ny < 0 or nx < 0 or ny >= N or nx >= M:
-            continue
-        if graph[ny][nx] == FRESH:
-            graph[ny][nx] = RIPEN
-            left -= 1
+    day += 1
+    for _ in range(len(queue)):
+        y, x = queue.popleft()
+        for dy, dx in zip(DY, DX):
+            ny = y+dy
+            nx = x+dx
+            if not (0 <= nx < M and 0 <= ny < N):
+                continue
+            if not should_be_visited[ny, nx]:
+                continue
+            should_be_visited[ny, nx] = False
+            raw_tomato_count -= 1
             queue.append((ny, nx))
 
-    width -= 1
-    if width == 0:
-        width = len(queue)
-        depth += 1
-
-# 더 이상 새로 익는 토마토가 없는데, 아직 덜 익은 토마토가 있는지 검사한다.
-if left > 0:
+if raw_tomato_count > 0:
     print(-1)
 else:
-    print(depth)
+    print(day)
