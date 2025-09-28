74d0c2296237ff877fd558167f5113beda101bbf
Hepheir <hepheir@gmail.com>
2025-09-29 04:08:34 +0900

    AC(108ms): O((N/2)!) #geometry #backtracking
    [P5]
    대각선 좌표 계산하기 너무 귀찮아서 그냥 좌표들을 45도 회전시켜놓고 풀었어요.
    
    ---
    
    시간제한이 넉넉한 것을 보고 백트래킹으로 접근해야겠다는 생각은 했으나, 대각선의 대푯값을 설정해서 비숍의 배치 가능 여부를 빠르게 파악하는 과정을 설계하는게 귀찮아서...
    
    그냥 회전행렬로 45도 좌표를 회전시켜놓고 편하게 y축을 따라 백트래킹하며 x 축 사용여부만 검사했다.
    
    비숍은 같은 색상의 칸에만 영향을 줄 수 있으므로, 색깔별로 경우의 수를 구분해서 정답을 구해냈다.
    (시간 단축 6488ms -> 108ms)

--- problem/1799/Main.py
+++ problem/1799/Main.py
@@ -1,44 +1,43 @@
 # 1799번: 비숍
 
-from collections import defaultdict
-from typing import *
+from typing import List, Set
 import sys
 
+INV_SQRT_2 = 1/(2**0.5)
 
-SQRT_2 = 2**0.5
-INV_SQRT_2 = 1/SQRT_2
+
+def backtracking(y_stack: List[List[int]], x_used: Set[float], max_count: int = 0) -> int:
+    if not y_stack:
+        return max(max_count, len(x_used))
+    if len(x_used)+len(y_stack) <= max_count:
+        return max_count
+    row = y_stack.pop()
+    max_count = max(max_count, backtracking(y_stack, x_used, max_count))
+    for x in row:
+        if x not in x_used:
+            x_used.add(x)
+            max_count = max(max_count, backtracking(y_stack, x_used, max_count))
+            x_used.discard(x)
+    y_stack.append(row)
+    return max_count
 
 
 N = int(sys.stdin.readline())
-grid = defaultdict(list)
+y_stacks = dict()
 for y in range(N):
     for x, is_placable in enumerate(map(int, sys.stdin.readline().split())):
+        rx = INV_SQRT_2*(x-y)
+        ry = INV_SQRT_2*(x+y)
+        if x >= N:
+            continue
+        if ry not in y_stacks:
+            y_stacks[ry] = list()
         if is_placable:
-            grid[INV_SQRT_2*(x+y)].append(INV_SQRT_2*(x-y))
+            y_stacks[ry].append(rx)
 
-
-y_stack: List[float] = list(grid.keys())
-x_used: Set[float] = set()
+y_stacks = [y_stacks[y] for y in sorted(y_stacks)]
 
 answer = 0
-
-
-def backtracking():
-    global answer
-    if not y_stack:
-        answer = max(answer, len(x_used))
-        return
-    if len(x_used)+len(y_stack) <= answer:
-        return
-    y = y_stack.pop()
-    backtracking()
-    for x in grid[y]:
-        if x not in x_used:
-            x_used.add(x)
-            backtracking()
-            x_used.discard(x)
-    y_stack.append(y)
-
-
-backtracking()
+answer += backtracking(y_stacks[0::2], set()) # 햐양 칸
+answer += backtracking(y_stacks[1::2], set()) # 검은 칸
 print(answer)
