7cc9a83c81696cd00a8edd66823d610d5f25f8da
Hepheir <hepheir@gmail.com>
2025-10-03 05:36:07 +0900

    TLE: O(N^2) #dp #game_theory

--- problem/15490/Main.py
+++ problem/15490/Main.py
@@ -10,21 +10,20 @@ sys.setrecursionlimit(10*MAX_N)
 
 
 N = int(sys.stdin.readline())
-A = list(n for n in map(int, sys.stdin.readline().split()) if n % 2 == 1)
-N = len(A)
+A = list(map(int, sys.stdin.readline().split()))
 
 
 @functools.lru_cache(maxsize=128*(1024**2))
 def can_win(s: int = 0, e: int = N, p1_is_even: bool = 1, p2_is_even: bool = 1) -> bool:
     if s == e:
         return p1_is_even
-    if (s+1 < e) and not can_win(s+2, e, p2_is_even, p1_is_even ^ (((A[s]+A[s+1]) % 2) & 1)):
-        return True
     if not can_win(s+1, e, p2_is_even, p1_is_even ^ ((A[s] % 2) & 1)):
         return True
-    if (s < e-1) and not can_win(s, e-2, p2_is_even, p1_is_even ^ (((A[e-2]+A[e-1]) % 2) & 1)):
+    if (s+1 < e) and not can_win(s+2, e, p2_is_even, p1_is_even ^ (((A[s]+A[s+1]) % 2) & 1)):
         return True
     if not can_win(s, e-1, p2_is_even, p1_is_even ^ ((A[e-1] % 2) & 1)):
+        return True
+    if (s < e-1) and not can_win(s, e-2, p2_is_even, p1_is_even ^ (((A[e-2]+A[e-1]) % 2) & 1)):
         return True
     return False
 
