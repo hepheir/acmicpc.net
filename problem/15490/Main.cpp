d85ef94afd92ab9673bef14d021caeced047b18c
Hepheir <hepheir@gmail.com>
2025-10-03 06:14:34 +0900

    AC(320ms): O(N^2) #dp #game_theory
    [P5]
    전형적인 게임이론에 DP를 덧붙여 풀이하는 문제... 지만 파이썬으로 풀기엔 시간제한이 매우 타이트하다.

--- problem/15490/Main.cpp
+++ problem/15490/Main.cpp
@@ -10,27 +10,27 @@ using namespace std;
 
 int N;
 int A[MAX_N];
-int cache[MAX_N + 1][MAX_N + 1][2][2];
+int cache[MAX_N][MAX_N][2][2];
 
-bool can_win(int s, int e, bool p1_is_even, bool p2_is_even)
+bool can_win(int s, int e, bool p1_is_odd, bool p2_is_odd)
 {
+    // e is exclusive
     if (s == e)
-        return p1_is_even;
-    if (cache[s][e][p1_is_even][p2_is_even] == CACHE_UNDETERMINED)
+        return !p1_is_odd;
+    if (cache[s][e][p1_is_odd][p2_is_odd] == CACHE_UNDETERMINED)
     {
-        cache[s][e][p1_is_even][p2_is_even] = CACHE_LOSE;
-        if (!can_win(s + 1, e, p2_is_even, p1_is_even ^ ((A[s] % 2) & 1)))
-            cache[s][e][p1_is_even][p2_is_even] = CACHE_WIN;
-        if ((s + 1 < e) && !can_win(s + 2, e, p2_is_even, p1_is_even ^ (((A[s] + A[s + 1]) % 2) & 1)))
-            cache[s][e][p1_is_even][p2_is_even] = CACHE_WIN;
-        if (!can_win(s, e - 1, p2_is_even, p1_is_even ^ ((A[e - 1] % 2) & 1)))
-            cache[s][e][p1_is_even][p2_is_even] = CACHE_WIN;
-        if ((s < e - 1) && !can_win(s, e - 2, p2_is_even, p1_is_even ^ (((A[e - 2] + A[e - 1]) % 2) & 1)))
-            cache[s][e][p1_is_even][p2_is_even] = CACHE_WIN;
+        cache[s][e][p1_is_odd][p2_is_odd] = CACHE_LOSE;
+        if ((s + 1 < e) && !can_win(s + 2, e, p2_is_odd, (p1_is_odd + A[s] + A[s + 1]) % 2))
+            cache[s][e][p1_is_odd][p2_is_odd] = CACHE_WIN;
+        if ((s < e - 1) && !can_win(s, e - 2, p2_is_odd, (p1_is_odd + A[e - 1] + A[e - 2]) % 2))
+            cache[s][e][p1_is_odd][p2_is_odd] = CACHE_WIN;
+        if (!can_win(s + 1, e, p2_is_odd, (p1_is_odd + A[s]) % 2))
+            cache[s][e][p1_is_odd][p2_is_odd] = CACHE_WIN;
+        if (!can_win(s, e - 1, p2_is_odd, (p1_is_odd + A[e - 1]) % 2))
+            cache[s][e][p1_is_odd][p2_is_odd] = CACHE_WIN;
     }
-    return cache[s][e][p1_is_even][p2_is_even];
+    return cache[s][e][p1_is_odd][p2_is_odd];
 }
-
 
 int main()
 {
@@ -39,9 +39,10 @@ int main()
     ios_base::sync_with_stdio(false);
 
     cin >> N;
-    for (int i = 0; i < N; ++i) cin >> A[i];
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
     memset(cache, CACHE_UNDETERMINED, sizeof(cache));
-    cout << (can_win(0, N, 0, 1) == CACHE_WIN ? "Yes" : "No") << endl;
+    cout << ((can_win(0, N, 0, 0) == CACHE_WIN) ? "Yes" : "No") << endl;
 
     return 0;
 }
