6c9df96ad3d8f6e76980049bf4d9cfae680cf937
Hepheir <hepheir@gmail.com>
2025-10-07 03:46:26 +0900

    AC(300ms): O(|N|) #mathematics #combinatorics #prefix_sum
    [P5]
    각 자리의 숫자가 몇 번, 어떤 숫자로 더해지는지에 중점을 두고 풀면 O(|N|)에 풀이가 가능하다.
    
    각 자리의 숫자가 몇 번 더해지냐를 보면, i가 1부터 시작할 때, 각 자리의 수는 왼쪽에서 2^(i-1)개를 고르거나, 오른쪽에서 11^(|N|-i)개를 고르는 경우가 있다.
    
    오른쪽에서 고르는 경우의 수는, j가 오른쪽에서 몇 개의 숫자를 고르는지 일 때 기준으로 다음과 같다.
    \sum_{j=0}^{|N|-i} C(|N|-i, j)*(10^j) = 11^{|N|-i}
    
    P.S. prefix sum으로 거듭제곱을 미리 구한 덕에 O(N log N) -> O(N) 으로 시간복잡도가 개선되였다. 수행시간은 약 3배 감소하였다.

--- problem/20505/Main.py
+++ problem/20505/Main.py
@@ -3,8 +3,15 @@
 MOD = 998244353
 
 N = input().strip()
+
+pow_2 = [1] * len(N)
+pow_11 = [1] * len(N)
+for i in range(1, len(N)):
+    pow_2[i] = (pow_2[i-1] * 2) % MOD
+    pow_11[i] = (pow_11[i-1] * 11) % MOD
+
 answer = 0
 for i, n in enumerate(map(int, N)):
-    answer += n * pow(2, i, MOD) * pow(11, len(N)-1-i, MOD)
+    answer += n * pow_2[i] * pow_11[len(N)-1-i]
     answer %= MOD
 print(answer)
