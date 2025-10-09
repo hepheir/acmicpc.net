dee55e36651f1f897b496b32ef060de3f8ade0e2
Hepheir <hepheir@gmail.com>
2025-10-10 00:58:05 +0900

    WA(4%): O(N log N) #greedy #priority_queue #dp(bottom-up)
    [G2]
    N개의 음식을 시킬 때, N-1개의 음식은 B 배열에서 가장 작은 순으로 고르고, 여기서 고르지 않은 음식 중 A에서 가장 작은 것으로 고름.
    
    A를 먼저 고르냐, B를 먼저 고르냐에 차이를 둬봤지만 최적해를 찾지는 못함.

--- problem/2786/Main.py
+++ problem/2786/Main.py
@@ -2,6 +2,11 @@
 
 import sys
 
+MAX_N = int(5e5)
+MAX_A = MAX_B = int(1e9)
+
+INF = MAX_A * MAX_N
+
 
 N = int(sys.stdin.readline())
 A = [0] * N
@@ -9,22 +14,34 @@ B = [0] * N
 for i in range(N):
     A[i], B[i] = map(int, sys.stdin.readline().split())
 
+
+# dp[n]: n개의 음식을 시키기 위한 최소 가격.
+dp = [INF] * (N+1)
+
 pq_A = sorted(range(N), key=lambda i: A[i], reverse=True)
 pq_B = sorted(range(N), key=lambda i: B[i], reverse=True)
-used = [False] * N
 
-answers = [-1] * (N+1)
-answers[1] = A[pq_A[-1]]
+dp[1] = A[pq_A[-1]]
+
+# A를 먼저 뽑는 경우:
+acc = 0
 for n in range(2, N+1):
-    answers[n] = answers[n-1]
+    if pq_B[-1] == pq_A[-1]:
+        pq_B.pop()
+    acc += B[pq_B.pop()]
+    dp[n] = min(dp[n], A[pq_A[-1]] + acc)
 
+pq_B = sorted(range(N), key=lambda i: B[i], reverse=True)
+
+# B를 먼저 뽑는 경우:
+used = [False] * N
+acc = 0
+for n in range(2, N+1):
+    acc += B[pq_B[-1]]
     used[pq_B[-1]] = True
-    answers[n] -= A[pq_A[-1]]
-    answers[n] += B[pq_B[-1]]
     pq_B.pop()
-
     while used[pq_A[-1]]:
         pq_A.pop()
-    answers[n] += A[pq_A[-1]]
+    dp[n] = min(dp[n], A[pq_A[-1]] + acc)
 
-sys.stdout.write('\n'.join(map(str, answers[1:N+1])))
+sys.stdout.write('\n'.join(map(str, dp[1:N+1])))
