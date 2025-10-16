ec12904713e81677a714d580fc7d447ea84fea3c
Hepheir <hepheir@gmail.com>
2025-10-17 03:59:03 +0900

    WA: O(1) #mathematics
    [S1]
    엘리베이터의 최적의 탑승 위치를 찾아가는 풀이.

--- problem/23784/Main.py
+++ problem/23784/Main.py
@@ -1,6 +1,5 @@
 # 23784번: Eidam-Sand Lair
 
-from math import ceil
 import sys
 
 
@@ -12,23 +11,29 @@ def main():
         print(int(answer))
 
 
-def solve(Yp: float, Lp: int, Ys: int, Ls: int) -> float:
-    if Yp == Lp:
-        return Yp * min(Ys, Ls)
-    if Yp == 0:
-        return 0
-    Ld = ceil(Yp) - Lp
-    Lt = abs(Ld) * Ls
-    if Yp * Ys <= Lt:
-        # 걸어 올라가는게 낫다.
-        answer = Yp * Ys
-    elif Ys > Lt+Ls:
-        # 기다리는게 낫다.
-        answer = solve(ceil(Yp), Lp+Ld, Ys, Ls) + Lt
-    else:
-        # 엘리베이터가 Yp로 올 동안 걸어 올라가자.
-        answer = solve(Yp - Lt / Ys, Lp + Ld, Ys, Ls) + Lt
-    return answer
+def solve(Yp: int, Lp: int, Ys: int, Ls: int) -> float:
+    # x층에서 엘리베이터를 탑승한다고 하면, 최적의 탑승 위치 x는...
+    # abs(Yp-x)*Ys = abs(Lp-x)*Ls
+    # (Yp^2 - 2*x*Yp + x^2) * Ys^2 = (Lp^2 - 2*x*Lp + x^2) * Ls^2
+    # x^2*(Ys^2 - Ls^2) + 2*x*(Lp*Ls^2 - Yp*Ys^2) + Yp^2*Ys^2 - Lp^2*Ls^2 = 0
+    x_candidates = []
+    if Ys-Ls != 0:
+        x_candidates.append((Yp*Ys-Lp*Ls)//(Ys-Ls))
+    if Ys+Ls != 0:
+        x_candidates.append(((Yp*Ys+Lp*Ls)//(Ys+Ls)))
+    ans_candidates = [
+        Yp*Ys,
+        calc_time(Yp, Lp, Ys, Ls, 0),
+        calc_time(Yp, Lp, Ys, Ls, Yp),
+        calc_time(Yp, Lp, Ys, Ls, Lp),
+        *(calc_time(Yp, Lp, Ys, Ls, x) for x in x_candidates),
+    ]
+    return min(ans_candidates)
+
+
+def calc_time(Yp: int, Lp: int, Ys: int, Ls: int, x: int) -> int:
+    """x층에서 엘리베이터를 타고 가면 걸리는 시간"""
+    return max(abs(Yp-x)*Ys, abs(Lp-x)*Ls) + x*min(Ys, Ls)
 
 
 if __name__ == '__main__':
