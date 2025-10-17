a65f8e55a11aaceec99acfc707b682945f1bd038
Hepheir <hepheir@gmail.com>
2025-10-17 23:12:10 +0900

    AC(32ms): O(1) #mathematics
    [B5]
    어떻게 해도 1이 나오는 군요...

---
+++ problem/18096/Main.py
@@ -0,0 +1,16 @@
+# 18096번: Арифметическая магия
+
+"""
+cases = set()
+for N in range(100):
+    for a in range(100):
+        for b in range(100):
+            x = (a+1) * (b+1) - a - b - a*b
+            y = x**N
+            print(x)
+            cases.add(y)
+
+print('y = ', cases)
+"""
+
+print(1)
