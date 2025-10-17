a75cce55a63fd8064e23a6146933a72e867843b3
Hepheir <hepheir@gmail.com>
2025-10-17 22:49:09 +0900

    WA: O(K) #case_works #greedy

--- problem/28616/Main.py
+++ problem/28616/Main.py
@@ -12,59 +12,60 @@ def main():
 
 
 def solve(K: int, S: str) -> str:
-    digits = {0: [], 1: [], 2: []}
-    count = {0: 0, 1: 0, 2: 0}
-    for i in range(K):
-        digit = int(S[i])
-        digits[digit % 3].append((digit, i))
-        count[digit % 3] += 1
-    for r in range(3):
-        digits[r].sort()
+    digits = {
+        0: [],
+        1: [],
+        2: [],
+    }
+    for i, n in enumerate(map(int, S)):
+        digits[n % 3].append((n, i))
 
     def bigint_key(s: str):
         return len(s), s
 
-    def solve_util(rm_0: int, rm_1: int, rm_2: int) -> str:
-        string = list(S)
-        for i in range(rm_0):
-            string[digits[0][i][1]] = ''
-        for i in range(rm_1):
-            string[digits[1][i][1]] = ''
-        for i in range(rm_2):
-            string[digits[2][i][1]] = ''
-        return ''.join(string)
+    def remove_char(S: str, rem: int) -> str:
+        j = -1
+        for i in range(len(S)):
+            n = int(S[i])
+            if n % 3 != rem:
+                continue
+            j = i
+            if i+1 < len(S) and int(S[i]) <= int(S[i+1]):
+                break
+        assert j != -1
+        return S[:j] + S[j+1:]
+
 
     ans = []
-    rem = (count[1] + 2*count[2]) % 3
+    rem = (len(digits[1]) + 2*len(digits[2])) % 3
     if rem == 0:
-        if count[0] >= 1:
+        if len(digits[0]) >= 1:
             # 0을 1개 제거.
-            ans.append(solve_util(1, 0, 0))
-        if count[1] >= 3:
-            # 1를 3개 제거.
-            ans.append(solve_util(0, 3, 0))
-        if count[2] >= 3:
-            # 2를 3개 제거.
-            pass
-        if count[1] >= 1 and count[2] >= 1:
+            ans.append(remove_char(S, 0))
+        if len(digits[1]) >= 1 and len(digits[2]) >= 1:
             # 1를 1개, 2를 1개 제거.
-            ans.append(solve_util(0, 1, 1))
+            ans.append(remove_char(remove_char(S, 1), 2))
+        if len(digits[1]) >= 3:
+            # 1를 3개 제거.
+            ans.append(remove_char(remove_char(remove_char(S, 1), 1), 1))
+        if len(digits[2]) >= 3:
+            # 2를 3개 제거.
+            ans.append(remove_char(remove_char(remove_char(S, 2), 2), 2))
     if rem == 1:
-        if count[1] >= 1:
+        if len(digits[1]) >= 1:
             # 1을 1개 제거.
-            ans.append(solve_util(0, 1, 0))
-        if count[2] >= 2:
+            ans.append(remove_char(S, 1))
+        if len(digits[2]) >= 2:
             # 2를 2개 제거.
-            ans.append(solve_util(0, 0, 2))
+            ans.append(remove_char(remove_char(S, 2), 2))
     if rem == 2:
-        if count[1] >= 2:
-            # 1을 2개 제거.
-            ans.append(solve_util(0, 2, 0))
-        if count[2] >= 1:
+        if len(digits[2]) >= 1:
             # 2를 1개 제거.
-            ans.append(solve_util(0, 0, 1))
-
-    return max(ans, key=bigint_key)
+            ans.append(remove_char(S, 2))
+        if len(digits[1]) >= 2:
+            # 1을 2개 제거.
+            ans.append(remove_char(remove_char(S, 1), 1))
+    return max(ans, default='0', key=bigint_key)
 
 
 if __name__ == '__main__':
