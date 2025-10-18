49111140dc6d7fa1a4098435c5511a1ce4d8bf56
Hepheir <hepheir@gmail.com>
2025-10-19 04:11:07 +0900

    PA(40/100): O(NM) #arithematic #arbitary_precision

--- problem/22193/Main.py
+++ problem/22193/Main.py
@@ -1,6 +1,18 @@
 # 22193번: Multiply
 
 N, M = map(int, input().split())
-A = int(input())
-B = int(input())
-print(A * B)
+A = list(map(int, input().strip()[::-1]))
+B = list(map(int, input().strip()[::-1]))
+C = [0] * (N + M)
+for i in range(N):
+    for j in range(M):
+        C[i + j] += A[i] * B[j]
+        for k in range(i + j, N + M):
+            if C[k] < 10:
+                break
+            C[k + 1] += C[k] // 10
+            C[k] %= 10
+answer = ''.join(map(str, C[::-1])).lstrip('0')
+answer = answer if answer else '0'
+
+print(answer)
