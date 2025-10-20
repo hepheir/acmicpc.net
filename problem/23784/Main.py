b6e4cf3e18feb076ab266a31042edee57b03622a
Hepheir <hepheir@gmail.com>
2025-10-21 05:15:11 +0900

    AC(40ms): O(1) #case_work #math
    [S5]
    엘리베이터 탑승 시점/여부에 따른 3가지 경우만 고려하면 되는 문제.
    
    ---
    
    이게 뭐라고 그렇게 틀렸을까...

--- problem/23784/Main.py
+++ problem/23784/Main.py
@@ -12,32 +12,10 @@ def main():
 
 
 def solve(Yp: int, Lp: int, Ys: int, Ls: int) -> int:
-    if Ys <= Ls:
-        return Yp*Ys
-
-    def take_ev_at(f: int) -> int:
-        return max(abs(Yp-f)*Ys, abs(Lp-f)*Ls) + abs(f)*Ls
-
-    # 경사 하강법...
-    lo = 0
-    hi = Yp
-    while lo < hi:
-        mid = (lo+hi)//2
-        df = take_ev_at(mid)-take_ev_at(mid-1)
-        if df <= 0:
-            lo = mid+1
-        else:
-            hi = mid-1
-
     return min(
-        # 처음 층부터 걸어 올라가는 시간
-        Yp*Ys,
-        # 처음 층에서 엘리베이터를 누르고 기다리는 시간
-        abs(Yp-Lp)*Ls+Yp*Ls,
-        # 어느정도 걸어 올라간 뒤에 엘베 호출
-        take_ev_at(lo-1),
-        take_ev_at(lo),
-        take_ev_at(lo+1),
+        Yp*Ys,                  # 처음부터 걸어감
+        max(Lp, Yp+Yp-Lp)*Ls,   # 처음부터 엘리베이터 탑승
+        abs(Yp-Lp)*Ys+Lp*Ls,    # 걸어서 내려간 후 엘리베이터 탑승
     )
 
 
