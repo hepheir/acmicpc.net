114457f25ebe9cee01774be092a386309d97e6ff
Hepheir <hepheir@gmail.com>
2025-11-05 05:58:17 +0900

    AC(100ms): O(N) #implementation
    [B5]
    문자열 N번 반복 출력하기

---
+++ problem/32384/Main.java
@@ -0,0 +1,14 @@
+// 32384번: 사랑은 고려대입니다
+
+import java.io.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            System.out.print("LoveisKoreaUniversity ");
+        }
+    }
+}
