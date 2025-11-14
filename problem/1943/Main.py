44ee7f6feb0fbe11aaeb8e9f13adf000561507ee
Hepheir <hepheir@gmail.com>
2025-11-14 19:25:22 +0900

    AC(1520ms, PyPy3) O((동전의 개수)^2) #dp #knapsack
    [G2]
    동전 금액의 합이 100,000원인 제한은 있으나 동전의 개수가 10,000개 이하라는 것을 증명할 수 없어서 믿음으로 풀어야 했던 문제.

--- problem/1943/Main.py
+++ problem/1943/Main.py
@@ -1,45 +1,32 @@
-from typing import *
-from collections import deque
+from typing import List, Tuple
 import sys
 
 
 MAX_CAPACITY = 100000
 
-queue = deque()
-visited = [False] * (MAX_CAPACITY+1)
+
+def solve(coins: List[Tuple[int, int]], half: int) -> bool:
+    if half != int(half):
+        return False
+    half = int(half)
+    dp = [False] * (half+1)
+    dp[0] = 1
+    for unit, count in coins:
+        for i in reversed(range(unit, half+1)):
+            if not dp[i-unit]:
+                continue
+            for j in range(count):
+                if i+unit*j > half:
+                    break
+                dp[i+unit*j] = True
+    return dp[half]
+
 
 for _ in range(3):
     N = int(sys.stdin.readline())
-
-    queue.clear()
-    for n in range(N//2+1):
-        visited[n] = False
-    queue.append(0)
-    visited[0] = True
-
     coins = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    coins.sort(reverse=True)
-
-    capacity = [unit*count for unit, count in coins]
-    capacity_ps = capacity.copy() # prefix sum
-    for i in range(1, N):
-        capacity_ps[i] += capacity_ps[i-1]
-
-    if capacity_ps[N-1] % 2 == 0:
-        for i in range(N):
-            unit, count = coins[i]
-            for _ in range(count):
-                for _ in range(len(queue)):
-                    x = queue.popleft()
-                    if (capacity_ps[N-1] - capacity_ps[i]) <= x <= (capacity_ps[N-1]):
-                        queue.append(x)
-                    y = x+unit
-                    if (capacity_ps[N-1] - capacity_ps[i]) <= y <= (capacity_ps[N-1]):
-                        if not visited[y]:
-                            visited[y] = True
-                            queue.append(y)
-
-    if visited[capacity_ps[N-1]//2]:
+    half = sum(unit*count for unit, count in coins) / 2
+    if solve(coins, half):
         sys.stdout.write("1\n")
     else:
         sys.stdout.write("0\n")
