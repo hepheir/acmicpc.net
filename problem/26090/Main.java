c747f55ad730cd58a8c4e56b98baca95bf7040bc
Hepheir <hepheir@gmail.com>
2025-11-23 21:04:27 +0900

    AC(164ms): O(N^2) #sieve #prefix_sum
    [S2]
    에라토스테네스의 체 + 구간 합(prefix sum 응용)
    
    ---
    사전에 O(N log log N)에 구해둔 에라토스테네스의 체를 통해 O(1)에 소수 판별.
    
    사전에 O(N)에 prefix sum을 구해두어 O(1)에 구간 합을 구할 수 있도록 함.
    
    O(N^2) 개의 부분 수열이 각각 완전한 수열인지 O(1)에 검사.

--- problem/26090/Main.java
+++ problem/26090/Main.java
@@ -1,34 +1,34 @@
 // 26090번: 완전한 수열
 
 import java.io.*;
-import java.util.*;
-import java.util.stream.*;
 
 class Main {
     private static final int MAX_N = 500;
     private static final int MAX_A_VALUE = 2000;
     private static final BufferedReader br;
     private static final boolean[] isPrime;
+    private static final int[] sequence;
+    private static final int[] prefixSum;
 
     static {
         br = new BufferedReader(new InputStreamReader(System.in));
         isPrime = new boolean[MAX_A_VALUE * MAX_N + 1];
-        initIsPrime();
+        sequence = new int[MAX_N];
+        prefixSum = new int[MAX_N];
+        initIsPrime(MAX_A_VALUE * MAX_N + 1);
     }
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-        List<Integer> a = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        List<Integer> prefixSum = getPrefixSum(a);
+        String[] rawA = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            sequence[i] = Integer.parseInt(rawA[i]);
+        }
+        initPrefixSum(N);
         int answer = 0;
-        for (int length = 1; length < N; length++) {
-            if (!isPrime[length]) {
-                continue;
-            }
-            for (int offset = 0; offset + length - 1 < N; offset++) {
-                if (isPrime[getRangeSum(prefixSum, offset, offset + length - 1)]) {
+        for (int s = 0; s < N; s++) {
+            for (int e = s; e < N; e++) {
+                if (isPerfectSequence(s, e)) {
                     answer++;
                 }
             }
@@ -36,35 +36,41 @@ class Main {
         System.out.println(answer);
     }
 
-    private static void initIsPrime() {
-        isPrime[0] = false;
-        isPrime[1] = false;
-        for (int i = 2; i < isPrime.length; i++) {
+    private static Boolean isPerfectSequence(int s, int e) {
+        int length = e - s + 1;
+        int sum = getRangeSum(s, e);
+        return isPrime[length] && isPrime[sum];
+    }
+
+    private static Integer getRangeSum(int s, int e) {
+        if (s == 0) {
+            return prefixSum[e];
+        }
+        return prefixSum[e] - prefixSum[s - 1];
+    }
+
+    private static void initIsPrime(int size) {
+        for (int i = 0; i < 2 && i < size; i++) {
+            isPrime[i] = false;
+        }
+        for (int i = 2; i < size; i++) {
             isPrime[i] = true;
         }
-        for (int i = 2; i * i < isPrime.length; i++) {
+        for (int i = 2; i * i < size; i++) {
             if (!isPrime[i])
                 continue;
-            for (int j = i * i; j < isPrime.length; j += i) {
+            for (int j = i * i; j < size; j += i) {
                 isPrime[j] = false;
             }
         }
     }
 
-    private static Integer getRangeSum(List<Integer> prefixSum, int s, int e) {
-        if (s == 0) {
-            return prefixSum.get(e);
+    private static void initPrefixSum(int size) {
+        if (size > 0) {
+            prefixSum[0] = sequence[0];
         }
-        return prefixSum.get(e) - prefixSum.get(s - 1);
-    }
-
-    private static List<Integer> getPrefixSum(List<Integer> a) {
-        List<Integer> prefixSum = new ArrayList<>();
-        int sum = 0;
-        for (int i = 0; i < a.size(); i++) {
-            sum += a.get(i);
-            prefixSum.add(sum);
+        for (int i = 1; i < size; i++) {
+            prefixSum[i] = prefixSum[i - 1] + sequence[i];
         }
-        return prefixSum;
     }
 }
