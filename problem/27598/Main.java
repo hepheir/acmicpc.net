926dbd681578f312d741d48789fde031ec096b69
Hepheir <hepheir@gmail.com>
2025-11-28 01:07:15 +0900

    AC(1423ms): O(n) #lis
    [S1]
    a에 대한 value-index 매핑을 구성해서 b에 적용 후, 가장 마지막 원소부터 역방향으로 시작하는 연속 lis의 크기를 구하면 쉽게 답을 구할 수 있다.

---
+++ problem/27598/Main.java
@@ -0,0 +1,52 @@
+// 27598번: Beppa and SwerChat
+
+import java.io.*;
+import java.util.*;
+
+
+class Main {
+    private static final int MAX_N = 100000;
+    private static final BufferedReader br;
+    private static final int[] a;
+    private static final int[] b;
+
+    static {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        a = new int[MAX_N];
+        b = new int[MAX_N];
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            StringTokenizer st;
+            int n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                b[i] = Integer.parseInt(st.nextToken());
+            }
+            System.out.println(solve(n, a, b));
+        }
+    }
+
+    private static int solve(int n, int[] a, int[] b) {
+        int[] valueToIndex = new int[MAX_N+1];
+        for (int i = 0; i < n; i++) {
+            valueToIndex[a[i]] = i;
+        }
+        // LIS (Longest Increasing Subsequence)로 치환
+        int lisLength = 1;
+        for (int i = n-2; i >= 0; i--) {
+            if (valueToIndex[b[i]] < valueToIndex[b[i+1]]) {
+                lisLength++;
+            } else {
+                break;
+            }
+        }
+        return n - lisLength;
+    }
+}
