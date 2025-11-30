39413bae94c069df11c32bf5f7846cecc78c9543
Hepheir <hepheir@gmail.com>
2025-12-01 00:05:16 +0900

    AC(132ms): O(1) #implementation
    [B5]
    입력 데이터의 전제조건과 출력 요구사항만 읽으면 B5~B4 정도라고 생각합니다.
    
    ---
    
    지문에서 요구하는 개념만 보면 시간복잡도에 대한 이해여서 브론즈 상위~실버 하위를 줘야하나 싶지만, 입력데이터의 전제조건과 출력 요구사항만 보면 또 브론즈 하위권인...

---
+++ problem/23235/Main.java
@@ -0,0 +1,14 @@
+// 23235번: The Fastest Sorting Algorithm In The World
+
+import java.io.*;
+
+class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int caseNumber = 1;
+        while (!br.readLine().strip().equals("0")) {
+            System.out.printf("Case %d: Sorting... done!\n", caseNumber++);
+        }
+    }
+}
