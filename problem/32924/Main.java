2c86ff99eda26704ce54b83c9b170f294d316b2d
Hepheir <hepheir@gmail.com>
2025-12-02 13:50:31 +0900

    WA: O(n) #sorting #case_works
    [G5]
    h[i]가 낮은 i 순서대로 p[i]를 조회하여 비내림차순인지 검사.

--- problem/32924/Main.java
+++ problem/32924/Main.java
@@ -42,14 +42,28 @@ class Main {
         }
     }
 
-    private static int solveTestCase(final int count) {
+    private static int solveTestCase(final int n) {
         int answer = 0;
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < n; i++) {
             INDEX[i] = i;
         }
-        Arrays.sort(INDEX, 0, count, comparator);
-        for (int i = 0; i < count; i++) {
-            if (i > 0 && P[INDEX[i-1]] > P[INDEX[i]]) {
+        // H가 작은 순, 같다면 P가 작은 순으로 정렬.
+        Arrays.sort(INDEX, 0, n, comparator);
+        for (int i = 0; i < n; i++) {
+            // 뭘 해도 p -> h를 못 만드는 경우
+            if (P[INDEX[i]] < H[INDEX[i]]) {
+                return -1;
+            }
+            // 정렬된 INDEX 기준, 먼저 조회할 p가 더 낮은 h로 가야하는데
+            // 먼저 조회한 p가 직후의 p보다 높다면,
+            // INDEX가 h에 대해 오름차순으로 정렬되어 있으므로,
+            // 앞의 p를 내리면 뒤에 있는 p들도 내려와야 한다는 전제를 만족 못함.
+            if ((i+1 < n) && (P[INDEX[i]] > P[INDEX[i+1]])) {
+                return -1;
+            }
+            // p가 같으면 무조건 같이 erode 할 수 밖에 없는데, 목표해야할 h가 다른경우
+            // -> 절대 도달 못함.
+            if ((i+1 < n) && (P[INDEX[i]] == P[INDEX[i+1]]) && (H[INDEX[i]] != H[INDEX[i + 1]])) {
                 return -1;
             }
             answer = Math.max(answer, P[INDEX[i]] - H[INDEX[i]]);
