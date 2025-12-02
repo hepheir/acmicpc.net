a31beba879049c729f23c34c65e3561ace4f3c96
Hepheir <hepheir@gmail.com>
2025-12-02 14:22:55 +0900

    AC(600ms): O(n) #sorting #case_works #greedy
    [G3]
    p를 오름차순으로 조회하며 검증하는 로직.
    실패하는 경우를 잘 찾는게 난이도가 있다.
    
    검증만 잘 되었다면, 문제의 정답은 가장 많이 깎인 산이 기준.

--- problem/32924/Main.java
+++ problem/32924/Main.java
@@ -12,19 +12,7 @@ class Main {
     private static final Comparator<Integer> comparator = new Comparator<>() {
         @Override
         public int compare(Integer o1, Integer o2) {
-            if (P[o1] < P[o2]) {
-                return -1;
-            }
-            if (P[o1] > P[o2]) {
-                return 1;
-            }
-            if (H[o1] < H[o2]) {
-                return -1;
-            }
-            if (H[o1] > H[o2]) {
-                return 1;
-            }
-            return 0;
+            return Integer.compare(P[o1], P[o2]);
         }
     };
 
@@ -43,20 +31,25 @@ class Main {
     }
 
     private static int solveTestCase(final int n) {
+        // P가 작은 순으로 인덱스들을 정렬.
         for (int i = 0; i < n; i++) {
             INDEX[i] = i;
         }
-        // P가 작은 순, 같다면 H가 작은 순으로 정렬.
         Arrays.sort(INDEX, 0, n, comparator);
+
         int totalErosionCount = 0; // erode 된 횟수
         for (int i = 0; i < n; i++) {
             int requiredErosionCount = P[INDEX[i]] - H[INDEX[i]];
             if (requiredErosionCount < totalErosionCount) {
                 return -1;
             }
-            // 이번 건 내리면 안되는데, 다음 번 것이 동일 고도이지만 목표하는 고도가 다르면
-            // 절대 불가능.
-            if (i+1 < n && P[INDEX[i]] == P[INDEX[i+1]] && H[INDEX[i]] != H[INDEX[i + 1]]) {
+
+            if ((i + 1 < n) && (H[INDEX[i]] > H[INDEX[i + 1]])) {
+                return -1;
+            }
+
+            // 다음 번 것이 '과거 고도'가 동일하지만 '현재 고도'가 다르면, erode 횟수를 맞춰줄 수 없음.
+            if ((i + 1 < n) && (P[INDEX[i]] == P[INDEX[i + 1]]) && (H[INDEX[i]] != H[INDEX[i + 1]])) {
                 return -1;
             }
             totalErosionCount = Math.max(totalErosionCount, requiredErosionCount);
