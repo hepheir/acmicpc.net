9dabc059195aa21a1b210aa34f2301ecf2432e2b
Hepheir <hepheir@gmail.com>
2025-12-02 14:12:44 +0900

    WA: O(n) #sorting #case_works
    [G4]
    p가 낮은 순서대로 조회하며 검증하는 로직

--- problem/32924/Main.java
+++ problem/32924/Main.java
@@ -12,16 +12,16 @@ class Main {
     private static final Comparator<Integer> comparator = new Comparator<>() {
         @Override
         public int compare(Integer o1, Integer o2) {
-            if (H[o1] < H[o2]) {
-                return -1;
-            }
-            if (H[o1] > H[o2]) {
-                return 1;
-            }
             if (P[o1] < P[o2]) {
                 return -1;
             }
             if (P[o1] > P[o2]) {
+                return 1;
+            }
+            if (H[o1] < H[o2]) {
+                return -1;
+            }
+            if (H[o1] > H[o2]) {
                 return 1;
             }
             return 0;
@@ -43,31 +43,24 @@ class Main {
     }
 
     private static int solveTestCase(final int n) {
-        int answer = 0;
         for (int i = 0; i < n; i++) {
             INDEX[i] = i;
         }
-        // H가 작은 순, 같다면 P가 작은 순으로 정렬.
+        // P가 작은 순, 같다면 H가 작은 순으로 정렬.
         Arrays.sort(INDEX, 0, n, comparator);
+        int totalErosionCount = 0; // erode 된 횟수
         for (int i = 0; i < n; i++) {
-            // 뭘 해도 p -> h를 못 만드는 경우
-            if (P[INDEX[i]] < H[INDEX[i]]) {
+            int requiredErosionCount = P[INDEX[i]] - H[INDEX[i]];
+            if (requiredErosionCount < totalErosionCount) {
                 return -1;
             }
-            // 정렬된 INDEX 기준, 먼저 조회할 p가 더 낮은 h로 가야하는데
-            // 먼저 조회한 p가 직후의 p보다 높다면,
-            // INDEX가 h에 대해 오름차순으로 정렬되어 있으므로,
-            // 앞의 p를 내리면 뒤에 있는 p들도 내려와야 한다는 전제를 만족 못함.
-            if ((i+1 < n) && (P[INDEX[i]] > P[INDEX[i+1]])) {
+            // 이번 건 내리면 안되는데, 다음 번 것이 동일 고도이지만 목표하는 고도가 다르면
+            // 절대 불가능.
+            if (i+1 < n && P[INDEX[i]] == P[INDEX[i+1]] && H[INDEX[i]] != H[INDEX[i + 1]]) {
                 return -1;
             }
-            // p가 같으면 무조건 같이 erode 할 수 밖에 없는데, 목표해야할 h가 다른경우
-            // -> 절대 도달 못함.
-            if ((i+1 < n) && (P[INDEX[i]] == P[INDEX[i+1]]) && (H[INDEX[i]] != H[INDEX[i + 1]])) {
-                return -1;
-            }
-            answer = Math.max(answer, P[INDEX[i]] - H[INDEX[i]]);
+            totalErosionCount = Math.max(totalErosionCount, requiredErosionCount);
         }
-        return answer;
+        return totalErosionCount;
     }
 }
