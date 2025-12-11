f17718c772d1af93d32b66c498774bbd69ea2dba
Hepheir <hepheir@gmail.com>
2025-12-12 04:19:37 +0900

    AC(36ms): O(NQ) #string #bruteforcing
    [S5]
    l, r 에 대하여 2중 루프로 브루트포싱해도 풀립니다.

--- problem/31846/Main.py
+++ problem/31846/Main.py
@@ -1,36 +1,20 @@
 # 31846번: 문자열 접기
 
-import functools
 import sys
 
 N = int(sys.stdin.readline())
-S = sys.stdin.readline().strip()
-
-
-@functools.lru_cache(maxsize=None)
-def get_score(s: int, e: int) -> int:
-    """
-    s: inclusive
-    e: exclusive
-    """
-    assert s < e
-    assert (e-s) % 2 == 0
-    score = 0
-    while s < e:
-        if S[s] == S[e-1]:
-            score += 1
-        s += 1
-        e -= 1
-    return score
-
-
-P = int(sys.stdin.readline())
-for _ in range(P):
+S = ' '+sys.stdin.readline().strip()
+Q = int(sys.stdin.readline())
+for _ in range(Q):
     l, r = map(int, sys.stdin.readline().split())
-    answer = 0
-    for pivot in range(l+1, r):
-        size = min(pivot-l, r-pivot+1)
-        s = pivot-1-size
-        e = pivot-1+size
-        answer = max(answer, get_score(s, e))
-    print(answer)
+    max_score = 0
+    for s in range(l, r):
+        e = s+1
+        score = 0
+        while l <= s and e <= r:
+            if S[s] == S[e]:
+                score += 1
+            s -= 1
+            e += 1
+        max_score = max(max_score, score)
+    print(max_score)
