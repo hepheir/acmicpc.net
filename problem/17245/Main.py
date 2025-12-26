9907e28deaa0b8598e80f8779c200bb3e6b68311
Hepheir <hepheir@gmail.com>
2025-12-27 05:22:58 +0900

    AC(820ms): O(N^2) #prefix_sum #parametric_search
    [S2]
    기존 풀이에서 prefix sum 을 사용하여 수행시간 단축 (1152ms -> 820ms)

--- problem/17245/Main.py
+++ problem/17245/Main.py
@@ -11,18 +11,25 @@ for y in range(N):
     server_rack.extend(map(int, sys.stdin.readline().split()))
 server_rack.sort()
 
+server_rack_height_ps = server_rack.copy()
+for i in range(1, len(server_rack)):
+    server_rack_height_ps[i] += server_rack_height_ps[i-1]
+server_rack_height_ps.append(0)  # zero-padding for -1 index.
+
+n_servers = sum(server_rack)
+
 
 def n_available_servers(t: int) -> int:
     n_leq_racks = bisect.bisect_right(server_rack, t)
     n_gt_racks = len(server_rack)-n_leq_racks
-    return sum(server_rack[:n_leq_racks]) + t*n_gt_racks
+    return server_rack_height_ps[n_leq_racks-1] + t*n_gt_racks
 
 
 lo = 0
 hi = max(server_rack)
 while lo < hi:
     mid = (lo + hi) // 2
-    if n_available_servers(mid) >= (0.5 * sum(server_rack)):
+    if n_available_servers(mid) >= (0.5 * n_servers):
         hi = mid
     else:
         lo = mid + 1
