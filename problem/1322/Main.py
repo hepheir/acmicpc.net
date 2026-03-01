c4cd5a657833da8676019a4ee7661befa822182b
Hepheir <hepheir@gmail.com>
2026-03-01 18:08:32 +0900

    AC(36ms): O(log N) #bitmask #two_pointer
    [G4]
    X 비트가 0인 부분만 채워주면 되는 문제

--- problem/1322/Main.py
+++ problem/1322/Main.py
@@ -1,30 +1,17 @@
 # 1322번: X와 K
 
 
-def main():
-    X, K = map(int, input().split())
-    answer = solve(X, K+1, X.bit_length())
-    print(answer)
+def solve(X: int, K: int) -> int:
+    Y = 0
+    y_i = 0
+    for k_i in range(K.bit_length()):
+        while X & (1 << y_i) != 0:
+            y_i += 1
+        if K & (1 << k_i) != 0:
+            Y |= 1 << y_i
+        y_i += 1
+    return Y
 
 
-def solve(X: int, K: int, bit_len: int) -> int:
-    if K == 1:
-        return 0
-    if (count := get_count(X, bit_len)) < K:
-        return solve(X, K, bit_len+1)
-    if (count := get_count(X, bit_len-1)) < K:
-        Y = 1 << (bit_len-1)
-        return solve(X, K - count, bit_len-1) | Y
-    return solve(X, K, bit_len-1)
-
-
-def get_count(X: int, bit_len: int) -> int:
-    count = 1
-    for i in range(bit_len):
-        if X & (1 << i) == 0:
-            count <<= 1  # power of 2
-    return count
-
-
-if __name__ == '__main__':
-    main()
+X, K = map(int, input().split())
+print(solve(X, K))
